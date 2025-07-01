f458b01742ca0eb078550f3e9963d6eab2936430
kabiru kolawole <kolawolekabiru89@gmail.com>
2025-07-01 11:48:49 +0100

    Updated

--- BookApp/admin.py
+++ BookApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import BookModel
 
 # Register your models here.
+class BookModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'price')
+
+
+admin.site.register(BookModel, BookModelAdmin)
--- BookApp/api.py
+++ BookApp/api.py
@@ -5,6 +5,7 @@ from rest_framework import serializers
 # This file contains API views for managing books, including listing, creating,
 # updating, and deleting books using Django REST Framework's serializers and viewsets.
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 
 class BookModelSerializer(serializers.ModelSerializer):
@@ -73,3 +74,58 @@ class BookModelSerializer(serializers.ModelSerializer):
 class BookViewSet(ModelViewSet):
     queryset = BookModel.objects.all()
     serializer_class = BookModelSerializer
+    permission_classes = [IsAuthenticated]
+
+    # The list method is used to retrieve a list of books authored by the logged-in user.
+    # If the user is not authenticated, it returns a message indicating that authentication is required.
+    def list(self, request):
+        user = request.user
+        books = BookModel.objects.filter(author = user)
+        serializer = self.get_serializer(books, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        # name, price
+        data = request.data
+         #author
+        user = request.user
+
+        serializer = self.get_serializer(data=data)
+        if serializer.is_valid():
+            serializer.save(author = user)
+
+        return Response({
+            'message': "Book Created",
+        })
+
+
+    def update(self, request, pk):
+
+        book = BookModel.objects.get(id=pk)
+
+        if book.author == request.user:
+
+            data= request.data
+            serializer = self.get_serializer(instance=book, data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({
+                    'message': 'Book Updated Successfully'
+                })
+        return Response({
+            'message': 'You are not the author of this book'
+        })
+
+    def destory(self, request, pk):
+        
+        book = BookModel.objects.get(id=pk)
+
+        if book.author == request.user:
+            book.delete()
+            return Response({
+                'message': 'Book Deleted successfully'
+            })
+        
+        return Response({
+            'message': 'You are not authorized to delete this book'
+        }) 
